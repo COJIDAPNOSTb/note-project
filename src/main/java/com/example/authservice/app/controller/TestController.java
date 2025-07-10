523737971eaeed43b823330d29c6ac681115eee4
COJIDAPNOSTb <foxydendi@gmail.com>
2025-07-10 17:51:30 -0100

    Add JWT

---
+++ src/main/java/com/example/authservice/app/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.example.authservice.app.controller;
+
+import org.springframework.context.annotation.Role;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @Secured("ROLE_USER")
+    @GetMapping("/user")
+    public String testUser()
+    {
+        return "You is User!";
+    }
+
+    @GetMapping("/admin")
+    @Secured("ROLE_ADMIN")
+    public String testAdmin()
+    {
+        return "You is Admin!";
+    }
+}
